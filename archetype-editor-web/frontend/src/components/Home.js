ce1d5fd24f4fb4badf243d7e3e006b150fb7f7d3
ItsmeSonny <xAleeh@live.cl>
2019-09-10 22:13:06 -0300

    Inicio de Interfaz

--- archetype-editor-web/frontend/src/components/Home.js
+++ archetype-editor-web/frontend/src/components/Home.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { changeTitle } from '../actions/home';
-import { Button } from 'antd';
-
+import { Button,Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider} = Layout;
+const ButtonGroup =Button.Group;
 class Home extends Component {
+
 
     render(){
         return (
@@ -12,6 +15,94 @@ class Home extends Component {
                 <h2>Welcome to { this.props.title }</h2>
                 <Button type="primary" onClick={()=>{this.props.handlerChangeTitle('NewTitle')}}>Change Title</Button>
                 <Link to="/about">About</Link>
+                <Menu theme="dark" mode="inline">
+                    <Icon type="fire" />
+                    <Button type="link" ghost>
+                        File
+                    </Button>
+                    <Button type="link" ghost>
+                        Edit
+                    </Button>
+                    <Button type="link" ghost>
+                        Selection
+                    </Button>
+                    <Button type="link" ghost>
+                        Help
+                    </Button>
+                </Menu>
+                
+                <Layout>
+                <Sider width={200}>
+                    <Menu
+                    theme="dark"
+                    mode="inline"
+                    
+                    style={{ height: '100%', borderRight: 0 }}
+                    >
+                        <SubMenu
+                            key="cap1"
+                            title={
+                            <span>
+                                <Icon type="arrow-down" />
+                                Carpeta 1
+                            </span>
+                            }
+                        >
+                            <Menu.Item key="1">Archetype 1</Menu.Item>
+                            <Menu.Item key="2">Archetype 2</Menu.Item>
+                            <Menu.Item key="3">Archetype 3</Menu.Item>
+                            <Menu.Item key="4">Archetype 4</Menu.Item>
+                        </SubMenu>
+                        <SubMenu
+                            key="cap2"
+                            title={
+                            <span>
+                                <Icon type="arrow-down" />
+                                Carpeta 2
+                            </span>
+                            }
+                        >
+                            <Menu.Item key="5">Archetype 1</Menu.Item>
+                            <Menu.Item key="6">Archetype 2</Menu.Item>
+                            <Menu.Item key="7">Archetype 3</Menu.Item>
+                            <Menu.Item key="8">Archetype 4</Menu.Item>
+                        </SubMenu>
+                        <SubMenu
+                            key="Blocks"
+                            title={
+                            <span>
+                                <Icon type="arrow-down" />
+                                Bloques
+                            </span>
+                            }
+                        >
+                            <Menu.Item key="block1">Bloque1</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                    
+                    <Layout>
+                        <Header style={{ background: '#fff', padding: 0 }} >
+                            <Menu
+                                theme="dark"
+                                mode="horizontal"
+                                defaultSelectedKeys={['1']}
+                                style={{ lineHeight: '64px' }}
+                            >
+                                <Menu.Item key="1">Archetype1</Menu.Item>
+                                <Menu.Item key="2">Archetype2</Menu.Item>
+                            </Menu>
+                        </Header>
+                        
+                        <Content style={{ margin: '24px 16px 0' }}>
+                            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Workspace</div>
+                        </Content>
+                        
+                        <Footer style={{ textAlign: 'center' }}>
+                            Archetypes ©2019 Created by WorkOrWate
+                        </Footer>
+                    </Layout>
+                </Layout>
             </div>
         );
     }
