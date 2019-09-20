bb6b5dbefa8880103b7a22d08c9f92930ec7dcdd
ItsmeSonny <xAleeh@live.cl>
2019-09-19 23:04:52 -0300

    Avance de interfaz

--- archetype-editor-desktop/src/components/Home.js
+++ archetype-editor-desktop/src/components/Home.js
@@ -2,16 +2,178 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { changeTitle } from '../actions/home';
-import { Button } from 'antd';
+import { Button,Layout, Menu, Breadcrumb, Icon, Dropdown,notification} from 'antd';
+/*import Board from './Board'*/
+/*import Card from './Cards'*/
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider} = Layout;
+const ButtonGroup = Button.Group;
+
+
+const NotificationSave = () => {
+    notification.open({
+      message: 'Se ha Guardado correctamente',
+      description:
+        'Su archivo esta esta en su sistema.',
+    });
+};
+const MenuFile = (
+    <Menu theme="dark">
+        <Menu theme="dark">
+            <Menu.Item key="nwfile">New File</Menu.Item>
+            <Menu.Item key="opfile">OpenFile</Menu.Item>
+            <Menu.Item key="svsave"  onClick={NotificationSave}>Save</Menu.Item>
+            <Menu.Item key="svsaveas">Save As</Menu.Item>
+            <Menu.Item key="quit">Exit</Menu.Item>
+       </Menu>
+    </Menu>
+);
+const MenuEdit = (
+    <Menu theme="dark">
+        <Menu theme="dark">
+            <Menu.Item key="undo">Undo</Menu.Item>
+            <Menu.Item key="redo">Redo</Menu.Item>
+            <Menu.Item key="cut">Cut</Menu.Item>
+            <Menu.Item key="copy">Copy</Menu.Item>
+            <Menu.Item key="paste">Paste</Menu.Item>
+       </Menu>
+    </Menu>
+);
+const MenuSelection = (
+    <Menu theme="dark">
+        <Menu theme="dark">
+            <Menu.Item key="slc">Select All</Menu.Item>
+       </Menu>
+    </Menu>
+);
+const MenuHelp = (
+    <Menu theme="dark">
+        <Menu theme="dark">
+            <Menu.Item key="docu">Documentation</Menu.Item>
+            <Menu.Item key="about">About</Menu.Item>
+       </Menu>
+    </Menu>
+);
+
 
 class Home extends Component {
-
+    constructor(props){
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {collapsed:false};
+}
+    toggle() {
+        this.setState(state => ({ collapse: !state.collapse }));
+    }
     render(){
         return (
             <div>
                 <h2>Welcome to { this.props.title }</h2>
                 <Button type="primary" onClick={()=>{this.props.handlerChangeTitle('NewTitle')}}>Change Title</Button>
                 <Link to="/about">About</Link>
+                <Layout >
+                    <Menu theme="dark" mode="inline">
+                        <Icon type="fire" />
+                        <Dropdown overlay={MenuFile} trigger={['click']}>
+                            <Button type="link" ghost>
+                                File
+                            </Button>
+                        </Dropdown>
+                        
+                        <Dropdown overlay={MenuEdit} trigger={['click']}>
+                            <Button type="link" ghost>
+                                Edit
+                            </Button>
+                        </Dropdown>
+                        
+                        <Dropdown overlay={MenuSelection} trigger={['click']}>
+                            <Button type="link" ghost>
+                                Selection
+                            </Button>
+                        </Dropdown>
+                        <Dropdown overlay={MenuHelp} trigger={['click']}>
+                            <Button type="link" ghost>
+                                Help
+                            </Button>
+                        </Dropdown>
+                    </Menu>
+                </Layout>
+                
+                
+                <Layout>
+                    <Sider collapsible onClick={this.toggle} style={{ minHeight: '100vh' }}>
+                        <Menu
+                        theme="dark"
+                        mode="inline">
+                            <SubMenu
+                                key="cap1"
+                                title={
+                                    <span>
+                                        <Icon type="folder" />
+                                        <span>Carpeta 1</span>
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="1">Archetype 1</Menu.Item>
+                                <Menu.Item key="2">Archetype 2</Menu.Item>
+                                <Menu.Item key="3">Archetype 3</Menu.Item>
+                                <Menu.Item key="4">Archetype 4</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="cap2"
+                                title={
+                                <span>
+                                    <Icon type="folder" />
+                                    <span>Carpeta 2</span>
+                                </span>
+                                }
+                            >
+                                <Menu.Item key="5">Archetype 1</Menu.Item>
+                                <Menu.Item key="6">Archetype 2</Menu.Item>
+                                <Menu.Item key="7">Archetype 3</Menu.Item>
+                                <Menu.Item key="8">Archetype 4</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="Blocks"
+                                title={
+                                <span>
+                                    <Icon type="block" />
+                                    <span>Bloques</span>
+                                </span>
+                                }
+                            >
+                                <Menu.Item key="block1">Bloque1</Menu.Item>
+                                
+                                
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+                    
+                    <Layout>
+                        <Header style={{ background: '#fff', padding: 0 }} >
+                            <Menu
+                                theme="dark"
+                                mode="horizontal"
+                                defaultSelectedKeys={['1']}
+                                style={{ lineHeight: '64px' }}
+                            >
+                                <Menu.Item key="1">Archetype1</Menu.Item>
+                                <Menu.Item key="2">Archetype2</Menu.Item>
+                            </Menu>
+                        </Header>
+                        
+                        <Content style={{ margin: '12px 12px ' }}>
+                            <Layout style={{ padding: 24, background: '#fff', minHeight: '100vh' }}>
+                                Workspace
+                            </Layout>
+
+                        </Content>
+                        
+                        <Footer style={{ textAlign: 'center' }}>
+                            Archetypes ©2019 Created by WorkOrWate
+                        </Footer>
+                    </Layout>
+                </Layout>
             </div>
         );
     }
@@ -31,5 +193,5 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-  
+
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- archetype-editor-web/frontend/src/components/Home.js
+++ archetype-editor-web/frontend/src/components/Home.js
@@ -2,84 +2,152 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { changeTitle } from '../actions/home';
-import { Button,Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Button,Layout, Menu, Breadcrumb, Icon, Dropdown,notification} from 'antd';
+/*import Board from './Board'*/
+/*import Card from './Cards'*/
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider} = Layout;
-const ButtonGroup =Button.Group;
+const ButtonGroup = Button.Group;
+
+
+const NotificationSave = () => {
+    notification.open({
+      message: 'Se ha Guardado correctamente',
+      description:
+        'Su archivo esta esta en su sistema.',
+    });
+};
+const MenuFile = (
+    <Menu theme="dark">
+        <Menu theme="dark">
+            <Menu.Item key="nwfile">New File</Menu.Item>
+            <Menu.Item key="opfile">OpenFile</Menu.Item>
+            <Menu.Item key="svsave"  onClick={NotificationSave}>Save</Menu.Item>
+            <Menu.Item key="svsaveas">Save As</Menu.Item>
+            <Menu.Item key="quit">Exit</Menu.Item>
+       </Menu>
+    </Menu>
+);
+const MenuEdit = (
+    <Menu theme="dark">
+        <Menu theme="dark">
+            <Menu.Item key="undo">Undo</Menu.Item>
+            <Menu.Item key="redo">Redo</Menu.Item>
+            <Menu.Item key="cut">Cut</Menu.Item>
+            <Menu.Item key="copy">Copy</Menu.Item>
+            <Menu.Item key="paste">Paste</Menu.Item>
+       </Menu>
+    </Menu>
+);
+const MenuSelection = (
+    <Menu theme="dark">
+        <Menu theme="dark">
+            <Menu.Item key="slc">Select All</Menu.Item>
+       </Menu>
+    </Menu>
+);
+const MenuHelp = (
+    <Menu theme="dark">
+        <Menu theme="dark">
+            <Menu.Item key="docu">Documentation</Menu.Item>
+            <Menu.Item key="about">About</Menu.Item>
+       </Menu>
+    </Menu>
+);
+
+
 class Home extends Component {
-
-
+    constructor(props){
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {collapsed:false};
+}
+    toggle() {
+        this.setState(state => ({ collapse: !state.collapse }));
+    }
     render(){
         return (
             <div>
                 <h2>Welcome to { this.props.title }</h2>
                 <Button type="primary" onClick={()=>{this.props.handlerChangeTitle('NewTitle')}}>Change Title</Button>
                 <Link to="/about">About</Link>
-                <Menu theme="dark" mode="inline">
-                    <Icon type="fire" />
-                    <Button type="link" ghost>
-                        File
-                    </Button>
-                    <Button type="link" ghost>
-                        Edit
-                    </Button>
-                    <Button type="link" ghost>
-                        Selection
-                    </Button>
-                    <Button type="link" ghost>
-                        Help
-                    </Button>
-                </Menu>
+                <Layout >
+                    <Menu theme="dark" mode="inline">
+                        <Icon type="fire" />
+                        <Dropdown overlay={MenuFile} trigger={['click']}>
+                            <Button type="link" ghost>
+                                File
+                            </Button>
+                        </Dropdown>
+                        
+                        <Dropdown overlay={MenuEdit} trigger={['click']}>
+                            <Button type="link" ghost>
+                                Edit
+                            </Button>
+                        </Dropdown>
+                        
+                        <Dropdown overlay={MenuSelection} trigger={['click']}>
+                            <Button type="link" ghost>
+                                Selection
+                            </Button>
+                        </Dropdown>
+                        <Dropdown overlay={MenuHelp} trigger={['click']}>
+                            <Button type="link" ghost>
+                                Help
+                            </Button>
+                        </Dropdown>
+                    </Menu>
+                </Layout>
+                
                 
                 <Layout>
-                <Sider width={200}>
-                    <Menu
-                    theme="dark"
-                    mode="inline"
-                    
-                    style={{ height: '100%', borderRight: 0 }}
-                    >
-                        <SubMenu
-                            key="cap1"
-                            title={
-                            <span>
-                                <Icon type="arrow-down" />
-                                Carpeta 1
-                            </span>
-                            }
-                        >
-                            <Menu.Item key="1">Archetype 1</Menu.Item>
-                            <Menu.Item key="2">Archetype 2</Menu.Item>
-                            <Menu.Item key="3">Archetype 3</Menu.Item>
-                            <Menu.Item key="4">Archetype 4</Menu.Item>
-                        </SubMenu>
-                        <SubMenu
-                            key="cap2"
-                            title={
-                            <span>
-                                <Icon type="arrow-down" />
-                                Carpeta 2
-                            </span>
-                            }
-                        >
-                            <Menu.Item key="5">Archetype 1</Menu.Item>
-                            <Menu.Item key="6">Archetype 2</Menu.Item>
-                            <Menu.Item key="7">Archetype 3</Menu.Item>
-                            <Menu.Item key="8">Archetype 4</Menu.Item>
-                        </SubMenu>
-                        <SubMenu
-                            key="Blocks"
-                            title={
-                            <span>
-                                <Icon type="arrow-down" />
-                                Bloques
-                            </span>
-                            }
-                        >
-                            <Menu.Item key="block1">Bloque1</Menu.Item>
-                        </SubMenu>
-                    </Menu>
-                </Sider>
+                    <Sider collapsible onClick={this.toggle} style={{ minHeight: '100vh' }}>
+                        <Menu
+                        theme="dark"
+                        mode="inline">
+                            <SubMenu
+                                key="cap1"
+                                title={
+                                    <span>
+                                        <Icon type="folder" />
+                                        <span>Carpeta 1</span>
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="1">Archetype 1</Menu.Item>
+                                <Menu.Item key="2">Archetype 2</Menu.Item>
+                                <Menu.Item key="3">Archetype 3</Menu.Item>
+                                <Menu.Item key="4">Archetype 4</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="cap2"
+                                title={
+                                <span>
+                                    <Icon type="folder" />
+                                    <span>Carpeta 2</span>
+                                </span>
+                                }
+                            >
+                                <Menu.Item key="5">Archetype 1</Menu.Item>
+                                <Menu.Item key="6">Archetype 2</Menu.Item>
+                                <Menu.Item key="7">Archetype 3</Menu.Item>
+                                <Menu.Item key="8">Archetype 4</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="Blocks"
+                                title={
+                                <span>
+                                    <Icon type="block" />
+                                    <span>Bloques</span>
+                                </span>
+                                }
+                            >
+                                <Menu.Item key="block1">Bloque1</Menu.Item>
+                                
+                                
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
                     
                     <Layout>
                         <Header style={{ background: '#fff', padding: 0 }} >
@@ -94,8 +162,11 @@ class Home extends Component {
                             </Menu>
                         </Header>
                         
-                        <Content style={{ margin: '24px 16px 0' }}>
-                            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Workspace</div>
+                        <Content style={{ margin: '12px 12px ' }}>
+                            <Layout style={{ padding: 24, background: '#fff', minHeight: '100vh' }}>
+                                Workspace
+                            </Layout>
+
                         </Content>
                         
                         <Footer style={{ textAlign: 'center' }}>
@@ -122,5 +193,5 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-  
+
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
