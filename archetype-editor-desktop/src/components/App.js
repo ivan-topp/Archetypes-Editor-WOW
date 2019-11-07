6eb94bad72f194bc4e65f18c5f3151a11cc21273
Iván Topp <ivan.topp.sandoval@gmail.com>
2019-11-07 14:00:54 -0300

    fix: Empaquetado Electron.
    El error que se soluciona es el que ocurría al compilar la aplicación react (Desktop).
    
    Con esto se soluciona el problema al empaquetar la aplicación electro, la causa del problema era que el react-reouter provocaba este error al compilar la aplicación react, la cual es necesaria para empaquetar al app electron, por ende solo se decidió quitar el react-router puesto que no se estaba usando.

--- archetype-editor-desktop/src/components/App.js
+++ archetype-editor-desktop/src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Home from './Home';
 const { electron } = window;
@@ -28,10 +27,7 @@ class App extends Component {
   
    render(){
     return (
-      <BrowserRouter>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-      </BrowserRouter>
+      <Home />
     );
   }
 }
