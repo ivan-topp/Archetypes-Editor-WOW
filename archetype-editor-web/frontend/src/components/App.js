e91708ae9801343ad9df4c1e4a45aabdcdf484e1
Iván Topp <ivan.topp.sandoval@gmail.com>
2019-11-07 13:58:40 -0300

    fix: Se arregla error.
    El error que se soluciona es el que ocurría al compilar la aplicación react (web).

--- archetype-editor-web/frontend/src/components/App.js
+++ archetype-editor-web/frontend/src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Home';
 
 //import { DatePicker } from 'antd';
@@ -16,10 +15,7 @@ class App extends Component {
 
   render(){
     return (
-      <BrowserRouter>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-      </BrowserRouter>
+      <Home />
     );
   }
 }
